2f99317a3de8d1fb8245a09b17c6717a4c54c2b7
drimescodes <olamidebamigbola@student.oauife.edu.ng>
2023-05-04 01:39:26 +0100

    made the mobile and desktop hero sections fully responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import {heroColor1, heroColor2, heroColor1_2, heroGirl, messengerIcon, freelance
 
 const Hero = () => {
   return (
-    <div className="w-full bg-[#263959] relative ">
-      <div className="max-w-full w-[90%] m-auto md:flex justify-between  text-white  ">
+    <div className="w-full bg-[#263959] relative min-h-screen overflow-hidden">
+      <div className="max-w-full w-[90%] m-auto md:flex justify-between  text-white  mt-16 md:mt-0">
 
         <div className=' self-center md:w-1/2 '>
             <h1 className="font-bold text-5xl leading-[4rem] ">90% Clients Hire Freelancer with 
@@ -13,44 +13,69 @@ Better Reviews</h1>
              </button>
         </div>
           
+          {/* image section of hero*/}
         
-  <img src={heroColor1} alt="Color Image 1" className="hidden md:block z-0 h-[500px] absolute right-0 md:top-0" />
-  <img src={heroColor2} alt="Color Image 2" className="hidden md:block z-[2] h-[450px] absolute right-0 bottom-0 " />
-  <img src={heroGirl} alt="" className="z-[10] -pt-14 self-start" />
+  <img src={heroColor1} alt="Color Image 1" className="hidden md:block z-0 h-[500px] absolute  right-0 md:top-0" />
+  <img src={heroColor2} alt="Color Image 2" className="hidden md:block z-[2] h-[450px] absolute right-0  bottom-0  " />
+  <img src={heroGirl} alt="" className="z-[10] -pt-14 self-start relative mt-[5rem] ml-[3rem] md:mt-0" />
   <div className='overflow-hidden'>
-  <img src={heroColor1_2} alt="Color Image 1" className="md:hidden z-0 h-[500px] absolute right-0 top-[280px] " />
-  <img src={heroColor2} alt="Color Image 2" className="md:hidden z-[2] h-[250px] absolute right-[-.4px] bottom-0 " />
+  <img src={heroColor1_2} alt="Color Image 1" className="md:hidden z-0 h-[450px] absolute right-0 top-[30rem] " />
+  <img src={heroColor2} alt="Color Image 2" className="md:hidden z-[2] h-[250px] absolute right-[-.4px] bottom-[-1.2rem]" />
 </div>
 
-    <div className='z-10 absolute top-1/2 left-1/2 hidden md:block'>
+    <div className='z-10 absolute top-1/2 left-1/2  md:block'>
     <div className="flex flex-wrap justify-center items-center h-48 w-48 rounded-full ">
         
     <img
   src={freelancerIcon}
   alt=""
-  className="absolute transform -translate-x-1/2 -translate-y-1/2 freelancer-spin "
-  style={{ top: "-100%", left: "10%" }}
+  className="absolute transform -translate-x-1/2 -translate-y-1/2 freelancer-spin"
+  style={
+    window.innerWidth < 640
+      ? { top: "30%", left: "-100%" }
+      : window.innerWidth >= 640 && window.innerWidth < 1024
+      ? { top: "-75%", left: "20%" }
+      : { top: "-100%", left: "10%" }
+  }
 />
 
 <img
   src={igIcon}
   alt=""
   className="absolute transform -translate-x-1/2 -translate-y-1/2 ig-spin overflow-hidden"
-  style={{ left: "200%", top: "-120%" }}
+  style={
+    window.innerWidth < 640
+      ? { left: "40%", top: "40%" }
+      : window.innerWidth >= 640 && window.innerWidth < 1024
+      ? { left: "280%", top: "-100%" }
+      : { left: "250%", top: "-120%" }
+  }
 />
 
 <img
   src={clientIcon}
   alt=""
   className="absolute transform -translate-x-1/2 -translate-y-1/2 client-spin"
-  style={{ left: "180%", top: "40%" }}
+  style={
+    window.innerWidth < 640
+      ? { left: "20%", top: "150%" }
+      : window.innerWidth >= 640 && window.innerWidth < 1024
+      ? { left: "230%", top: "60%" }
+      : { left: "220%", top: "70%" }
+  }
 />
 
 <img
   src={messengerIcon}
   alt=""
   className="absolute transform -translate-x-1/2 -translate-y-1/2 messenger-spin"
-  style={{ top: "90%", left: "-20%" }}
+  style={
+    window.innerWidth < 640
+      ? { top: "180%", left: "-80%" }
+      : window.innerWidth >= 640 && window.innerWidth < 1024
+      ? { top: "80%", left: "-20%" }
+      : { top: "90%", left: "-30%" }
+  }
 />
 
 
