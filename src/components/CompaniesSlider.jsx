901e981f199dc936db6e5af0cc7e3f497b83748e
drimescodes <olamidebamigbola@student.oauife.edu.ng>
2023-05-07 14:05:37 +0100

    perfected the slider

--- src/components/CompaniesSlider.jsx
+++ src/components/CompaniesSlider.jsx
@@ -3,6 +3,7 @@ import { Behance } from "../assets";
 import { Instagram } from "../assets";
 import { Github } from "../assets";
 import { Stackoverflow } from "../assets";
+import "./CompaniesSlider.css"
 
 
 const CompaniesSlider = () => {
@@ -10,18 +11,18 @@ const CompaniesSlider = () => {
     { src: Dribbble, alt: "Dribbble" },
     { src: Behance, alt: "Behance" },
     { src: Instagram, alt: "Instagram" },
-    { src: Github, alt: "GitHub" },
+    { src: Github, alt: "Github" },
     { src: Stackoverflow, alt: "Stackoverflow" },
-    
   ];
+  
 
   return (
     <div className="w-full bg-white overflow-hidden">
-      <div className="max-w-full w-[90%] m-auto  py-8 md:py-12 ">
-        <div className="slider">
+      <div className="max-w-full w-[90%] m-auto py-8 md:py-12 ">
+        <div className="slider flex justify-between gap-10 md:gap-16 lg:gap-20 h-100% ">
           {companyLogos.map((logo, index) => (
-            <div key={index} className="slide">
-              <img src={logo.src} alt={logo.alt}  />
+            <div key={index} className="slide ">
+              <img src={logo.src} alt={logo.alt} />
             </div>
           ))}
         </div>
